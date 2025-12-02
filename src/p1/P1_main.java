7845075421ea2c0cdd9ad6dc60daab615cce03fe
Musawer Mohibzada <musawer@MacBook-Pro-von-Musawer.local>
2025-12-02 11:20:23 +0100

    P

--- src/p1/P1_main.java
+++ src/p1/P1_main.java
@@ -1,5 +1,43 @@
 package p1;
 
 public class P1_main {
+	public static void main ( String [] args) {
+		multFrac(5,2,3,4);
+		Bruch a = new Bruch(2,3);
+		Bruch b = new Bruch(5,2);
+		 System.out.println("Der Zähler ist: " + b.z);
+	     System.out.println("Der Nenner ist: " + b.n);
+	    multFrac(a,b);
+	        
+		
+	}
+	public static void multFrac (int z1, int n1, int z2, int n2) {
+		int ergebnisZ = z1 * z2;
+		int ergebnisN = n1 * n2;
+		
+		System.out.println(ergebnisZ + " : " + ergebnisN);
+		
+	}
+	
+	
+	public static class Bruch{
+		int z;
+		int n;
+		
+
+		
+		public Bruch (int zaehler, int nenner) {
+			this.z = zaehler;
+			this.n = nenner;
+		}
+		
+		
+	}
+	
+	public static void multFrac (Bruch a, Bruch b) {
+		System.out.println((a.z*b.z)+":"+(a.n*b.n));
+	}
+	
+	
 
 }
--- src/p2/P2_main.java
+++ src/p2/P2_main.java
@@ -1,5 +1,21 @@
 package p2;
 
 public class P2_main {
-
+	public static void main (String [] args) {
+		
+	}
+	
+	public class LetterGame {
+		String keyString = new String ("FRANZ JAGT IM KOMPLETT VERWAHRLOSTEN TAXI\n"
+				+ "QUER DURCH BAYERN!");
+		String word = new String ("");
+		
+		public void setWord(int [] a) {
+			for (int i = 0; i < a.length; i++) {
+				word += keyString.charAt(a[i]);
+				
+			}
+		}
+		
+	}
 }
